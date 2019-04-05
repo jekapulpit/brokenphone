955529bdeecd85dbb77286541cad297aede1323e
yauhen <yauhen@wse-181-71b.local>
2019-04-05 17:53:58 +0300

    added some references and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_141628) do
+ActiveRecord::Schema.define(version: 2019_04_05_142214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_04_05_141628) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sender_id"
+    t.integer "recipient_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(email: 'fcbb96@mail.ru', password: '123456', confirmed_at: DateTime.now)
+user2 = User.create(email: 'fcbb95@mail.ru', password: '123456', confirmed_at: DateTime.now)
+Message.create(content: 'sdasd', sender: user1, recipient: user2)
+Message.create(content: 'sddsaasdasd', sender: user1, recipient: user2)
+Message.create(content: 'sdaasdasdasdadsd', sender: user2, recipient: user1)
+Message.create(content: 'sdasasdasdasdasdasdd', sender: user2, recipient: user1)
