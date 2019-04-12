0dbc927b6018cbb5e86eafa78ad9becba83b39dd
yauhen <yauhen@wse-181-71b.local>
2019-04-12 18:27:14 +0300

    remaked components into stateless

--- app/javascript/components/rooms/Menu.jsx
+++ app/javascript/components/rooms/Menu.jsx
@@ -2,21 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import Profile from "./Profile";
 import RoomsList from "./RoomsList";
-class Menu extends React.Component {
-
-  constructor(props) {
-    super(props)
-  }
-
-  render () {
+const Menu = props => {
     return (
         <div className="menu">
-          <Profile user={this.props.user} avatar={this.props.avatar} />
+          <Profile user={props.user} avatar={props.avatar} />
           <h5>Свежие чаты:</h5>
-          <RoomsList activeId={this.props.activeId} handleRoom={this.props.handleRoom} rooms={this.props.rooms} dia1={this.props.dia1}/>
+          <RoomsList activeId={props.activeId} handleRoom={props.handleRoom} rooms={props.rooms} dia1={props.dia1}/>
         </div>
     );
-  }
-}
+};
 
 export default Menu
--- app/javascript/components/rooms/RoomsList.jsx
+++ app/javascript/components/rooms/RoomsList.jsx
@@ -1,22 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Room from "./Room";
-class RoomsList extends React.Component {
-
-  constructor(props) {
-    super(props)
-  }
-
-  render () {
-    let rooms = this.props.rooms.map((room) => {
-       return (  <Room active={this.props.activeId === room.id} room={room} handleRoom={this.props.handleRoom} key={room.id} dia1={this.props.dia1} />  )
+const RoomsList = props => {
+    let rooms = props.rooms.map((room) => {
+       return (  <Room active={props.activeId === room.id} room={room} handleRoom={props.handleRoom} key={room.id} dia1={props.dia1} />  )
     });
     return (
         <div className="dialogs">
             {rooms}
         </div>
     );
-  }
-}
+};
 
 export default RoomsList
