60ebef0782cb2f8bcc0f09f42b7ed2f8257732a7
yauhen <yauhen@wse-181-71b.local>
2019-04-25 13:43:20 +0300

    removed cipher checkout for a while

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def show
     @rooms = Room.all.map(&:with_last_message)
-    enc = Messages::EncryptOperation.new('privet', Room.last).execute
-    Messages::DecryptOperation.new(enc, Room.last).execute
   end
 end
