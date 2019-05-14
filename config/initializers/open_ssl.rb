b092439e4864a3738c7b14091999ccade25c90b2
zheka <q010@bk.ru>
2019-05-14 15:22:45 +0300

    fixed ssl

--- config/initializers/open_ssl.rb
+++ config/initializers/open_ssl.rb
@@ -1,2 +1,6 @@
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = :SSLv23
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] |= OpenSSL::SSL::OP_NO_SSLv3
+OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] = -> {
+        opts = OpenSSL::SSL::OP_NO_SSLv3
+        opts &= ~OpenSSL::SSL::OP_DONT_INSERT_EMPTY_FRAGMENTS
+        opts |= OpenSSL::SSL::OP_NO_COMPRESSION
+        opts
+}.call
